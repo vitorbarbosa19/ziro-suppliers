46bfcab7643bfb702106e6353022728b0afe8e21
VITOR BARBOSA <vitorbarbosa19@gmail.com>
2018-02-17 11:05:48 -0800

    add auth0 and start configuration

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,49 +1,28 @@
 import React, { Component } from 'react'
 import { Image } from 'cloudinary-react'
-import SvgUser from './icons/SvgUser'
-import SvgPass from './icons/SvgPass'
-import { box, container, title, info, field, icon, input, submit } from './styles'
+import auth from './auth'
+import { container, illustration, title, info, submit } from './styles'
 
 export default class Login extends Component {
+	login = () => {
+		const authLogin = new auth()
+		authLogin.login()
+	}
 	render() {
 		return(
-			<div style={box}>
-				<div style={container}>
-					<Image
-						style={{marginTop: '90px'}}
-			      cloudName='ziro'
-			      width={window.innerWidth > 500 ? '160' : '100'}
-			      publicId='icon-login_ep7dtu'
-			      version='1518827830'
-			      format='png'
-			      secure='true'
-		   		/>
-					<span style={title}>Gerenciador de Estoque</span>
-					<span style={info}>
-						Faça login para ter acesso ao gerenciador de estoque
-					</span>
-					<div style={field}>
-						<div style={icon}>
-							<SvgUser
-								width={20}
-								height={20}
-								color={'rgba(48,62,77,0.50)'}
-							/>
-						</div>
-						<input style={input} type='email' placeholder='Email' />
-					</div>
-					<div style={field}>
-						<div style={icon}>
-							<SvgPass
-								width={20}
-								height={20}
-								color={'rgba(48,62,77,0.50)'}
-							/>
-						</div>
-						<input style={input} type='password' placeholder='Senha' />
-					</div>
-					<button style={submit}>Login</button>
-				</div>
+			<div style={container}>
+				<Image
+					style={illustration}
+		      cloudName='ziro'
+		      width={window.innerWidth > 500 ? '160' : '100'}
+		      publicId='icon-login_ep7dtu'
+		      version='1518827830'
+		      format='png'
+		      secure='true'
+	   		/>
+				<span style={title}>Gerenciador de Estoque</span>
+				<span style={info}>Faça login para ter acesso ao gerenciador de estoque</span>
+				<button style={submit} onClick={this.login}>Login</button>
 			</div>
 		)
 	}
--- src/components/Login/styles.js
+++ src/components/Login/styles.js
@@ -1,14 +1,3 @@
-export const box = {
-	display: 'flex',
-	alignItems: 'center',
-	margin: '0 auto',
-	maxHeight: '600px', 
-	maxWidth: '550px',
-	borderRadius: '10px',
-	boxShadow: `2px 2px 8px 2px rgba(48,62,77,0.10), 4px 4px 10px 4px rgba(48,62,77,0.02)`,
-	backgroundColor: '#fdfdfd'
-}
-
 export const container = {
 	display: 'flex',
 	flexDirection: 'column',
@@ -18,8 +7,12 @@ export const container = {
 	maxWidth: '450px',
 	height: '700px',
 	borderRadius: '10px',
-	boxShadow: `2px 2px 8px 2px rgba(48,62,77,0.10), 4px 4px 10px 4px rgba(48,62,77,0.05)`,
+	boxShadow: `0px 0px 8px 2px rgba(48,62,77,0.10), 0px 0px 10px 4px rgba(48,62,77,0.05)`,
 	backgroundColor: 'white'
+}
+
+export const illustration = {
+	marginTop: '130px'
 }
 
 export const title = {
@@ -39,34 +32,7 @@ export const info = {
 	textAlign: 'center'
 }
 
-export const field = {
-	width: '70%',
-	height: '45px',
-	display: 'flex',
-	margin: '8px auto',
-	position: 'relative'
-}
-
-export const icon = {
-	position: 'absolute',
-	top: '12px',
-	left: '22px'
-}
-
-export const input = {
-	width: '100%',
-	border: '1px solid rgba(48,62,77,0.50)',
-	outline: 'none',
-	borderRadius: '30px',
-	padding: '0 15px 0 50px',
-	fontSize: '1.6rem',
-	fontFamily: 'Work Sans',
-	color: 'rgba(48,62,77,0.50)',
-	boxShadow: `0px 3px 10px 0px rgba(48,62,77,0.10), 0px 4px 12px 0px rgba(48,62,77,0.05)`,
-}
-
 export const submit = {
-	margin: '20px 5px 90px',
 	WebkitAppearance: 'none',
 	MozAppearance: 'none',
 	outline: 'none',
