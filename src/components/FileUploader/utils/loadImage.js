d7bca2827c17fc9ce090fb419d90bf606555921e
VITOR BARBOSA <vitorbarbosa19@gmail.com>
2018-03-11 21:15:00 -0700

    add to FileUploader component an integration to cloudinary upload api

--- src/components/FileUploader/utils/loadImage.js
+++ src/components/FileUploader/utils/loadImage.js
@@ -1,5 +1,15 @@
-const loadImage = (that) => async (reader) => {
-	console.log(reader)
+import axios from 'axios'
+
+const loadImage = (that) => async (file) => {
+	try {
+		const result = await axios.post('https://api.cloudinary.com/v1_1/ziro/image/upload', {
+			file: file,
+			upload_preset: 'default'
+		})
+		console.log(result)
+	} catch (error) {
+		console.log(error)
+	}
 }
 
 export default loadImage
