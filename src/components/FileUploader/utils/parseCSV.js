697e8b75297e585e9c4b994949b4e5d2fc9aa93c
VITOR BARBOSA <vitorbarbosa19@gmail.com>
2018-03-15 21:45:55 -0700

    start new parsing method for CSV files, where products with same id are grouped together

--- src/components/FileUploader/utils/parseCSV.js
+++ src/components/FileUploader/utils/parseCSV.js
@@ -1,14 +1,19 @@
 const parseCSV = (csvFile) => {
+  console.log(csvFile)
 	const [titles, ...rows] = csvFile.split('\n')
 	return rows.map( (product) => {
     const characteristics = titles.split(',')
     const values = product.split(',')
-    return Object.assign({}, ...values.map( (value, index) => {
-      const characteristic = characteristics[index].trim()
-      return {
-        [characteristic]: value.trim()
+    return {
+      [characteristics[0].trim()]: values[0].trim(),
+      [characteristics[1].trim()]: values[1].trim(),
+      [characteristics[2].trim()]: values[2].trim(),
+      Grade: {
+        [characteristics[3].trim()]: values[3].trim(),
+        [characteristics[4].trim()]: values[4].trim(),
+        [characteristics[5].trim()]: values[5].trim(),
       }
-    }))
+    }
   })
 }
 
