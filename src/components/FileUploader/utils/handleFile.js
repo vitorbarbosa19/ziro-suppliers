9f88f9f172e23c50fb8058a1da51b9de48e1db01
VITOR BARBOSA <vitorbarbosa19@gmail.com>
2018-03-21 10:33:30 -0700

    investigate production error during execution of handleFile function on windows 7

--- src/components/FileUploader/utils/handleFile.js
+++ src/components/FileUploader/utils/handleFile.js
@@ -3,12 +3,15 @@ import loadImage from './loadImage'
 
 const handleFile = (that) => () => {
 	[...that.uploadButton.files].map( (file) => {
-		if (/^text\/csv$/.test(file.type)) {
+		console.log(file)
+		console.log(file.type)
+		console.log(file.type.toLowerCase())
+		if (/^text\/csv$/.test(file.type.toLowerCase())) {
 			const reader = new FileReader()
 			reader.onload = loadCSV(that).bind(null, reader)
 			reader.readAsText(file)
 		}
-		if (/^image\/(png|jpeg)$/.test(file.type)) {
+		if (/^image\/(png|jpeg)$/.test(file.type.toLowerCase())) {
 			const reader = new FileReader()
 			reader.onload = loadImage(that).bind(null, reader, file)
 			reader.readAsDataURL(file)
